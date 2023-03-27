269d54d5c9088efb2d63abbce2d34478eb8609e4
whatasame <97666463+whatasame@users.noreply.github.com>
2023-03-27 22:57:32 +0900

    [Silver III] Title: 두 수의 합, Time: 228 ms, Memory: 26572 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /* Read input data */
+        final int N = Integer.parseInt(br.readLine());
+        final String sequence = br.readLine();
+        final int X = Integer.parseInt(br.readLine());
+
+        /* Init range number array */
+        boolean[] isExist = new boolean[2000001]; // 1 to 2,000,000
+
+        /* Count number pair */
+        int pairCount = 0;
+        StringTokenizer st = new StringTokenizer(sequence);
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            /* Find pair number */
+            int pair = X - num;
+            if (pair < 0) { // num > X
+                continue;
+            }
+            if (isExist[pair]) {
+                pairCount++;
+            }
+
+            /* Mark num */
+            isExist[num] = true;
+        }
+
+        /* Print result */
+        System.out.println(pairCount);
+
+        br.close();
+    }
+}
