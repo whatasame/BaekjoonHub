d0b145983e813bfbcf94899ece64021efc70869c
whatasame <whatasame@gmail.com>
2023-05-03 12:59:27 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,14 +1,9 @@
 N = int(input())
+
 dp = [0] * (N + 1)
+dp[0] = 0
+dp[1] = 1
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
 
-
-def fibo(n):
-    if n <= 1:
-        return n
-    if dp[n] != 0:
-        return dp[n]
-    dp[n] = fibo(n - 1) + fibo(n - 2)
-    return dp[n]
-
-
-print(fibo(N))
+print(dp[N])
