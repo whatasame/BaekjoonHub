4cb4aa9744b6224a069157e0e1ce38f6b980a562
whatasame <whatasame@gmail.com>
2023-05-16 21:26:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 6020 ms, Memory: 229220 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,16 @@
+# 0-1 knapsack problem
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    item = [(0, 0)] + [tuple(map(int, input().split())) for _ in range(N)]
+
+    dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        for k in range(1, K + 1):
+            w, v = item[i]  # weight, value
+            if w <= k:
+                dp[i][k] = max(dp[i - 1][k], dp[i - 1][k - w] + v)
+            else:
+                dp[i][k] = dp[i - 1][k]
+
+    print(dp[N][K])
