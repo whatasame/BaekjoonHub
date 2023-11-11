4be43cb5f4ffcc3d735b9c4dd295f70edfb9e72d
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-11-11 17:21:52 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fn1) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.11 ms
+메모리: 10.2 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 17:8:7
+2023년 11월 6일 17:21:50
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -10,15 +10,9 @@ def solution(places):
     
     # 모든 대기실에 대하여
     for place in places:
-        if check(place):
-            answer.append(1)
-        else:
-            answer.append(0)
+        answer.append(check(place))
             
     return answer
-    
-
-        
 
 def check(place):
     row_len, col_len = len(place), len(place[0])
@@ -27,12 +21,12 @@ def check(place):
     for r in range(row_len):
         for c in range(col_len):
             # 사람이면 BFS(상, 하, 좌, 우)로 가장 가까운 사람의 거리 찾기
-            # 거리가 2 이상이면 거리두기 X
+            # 거리가 2 이하이면 거리두기 X (거리 0 == 사람 없음)
             if place[r][c] == "P" and 1 <= bfs(place, r, c) <= 2:
-                    return False
+                    return 0
     
     # 거리두기 O
-    return True
+    return 1
         
 def bfs(place, row, col):
     row_len, col_len = len(place), len(place[0])
