2b82b41a49551afaf8f7a3161dee4b56c7e3b444
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-11-22 20:29:05 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.53 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -4,9 +4,14 @@
 
 # number 내 중복 번호 존재
 
+keypad = {1: (0, 0), 2: (0, 1), 3: (0, 2),
+          4: (1, 0), 5: (1, 1), 6: (1, 2),
+          7: (2, 0), 8: (2, 1), 9: (2, 2),
+          "*": (3, 0), 0: (3, 1), "#": (3, 2)}
+
 def solution(numbers, hand):
     # 손가락 초기화
-    left, right = (3, 0), (3, 2) 
+    left, right = keypad["*"], keypad["#"]
     
     answer = []
     for number in numbers:
@@ -16,39 +21,20 @@ def solution(numbers, hand):
     return "".join(map(str, answer))
 
 def move(number, left, right, hand):
-    keymap = {
-        1: (0, 0),
-        2: (0, 1),
-        3: (0, 2),
-        4: (1, 0),
-        5: (1, 1),
-        6: (1, 2),
-        7: (2, 0),
-        8: (2, 1),
-        9: (2, 2),
-        "*": (3, 0),
-        0: (3, 1),
-        "#": (3, 2),
-    }
-    
     if number in (1, 4, 7):
-        return keymap[number], right, "L"
+        return keypad[number], right, "L"
     if number in (3, 6, 9):
-        return left, keymap[number], "R"
+        return left, keypad[number], "R"
     
-    left_distance = abs(keymap[number][0] - left[0]) + abs(keymap[number][1] - left[1])
-    right_distance = abs(keymap[number][0] - right[0]) + abs(keymap[number][1] - right[1])
+    left_distance = abs(keypad[number][0] - left[0]) + abs(keypad[number][1] - left[1])
+    right_distance = abs(keypad[number][0] - right[0]) + abs(keypad[number][1] - right[1])
     
-    if left_distance == right_distance:
+    if left_distance < right_distance:
+        return keypad[number], right, "L"
+    if left_distance > right_distance:
+        return left, keypad[number], "R"
+    else:
         if hand == "left":
-            return keymap[number], right, "L"
+            return keypad[number], right, "L"
         elif hand == "right":
-            return left, keymap[number], "R"
-    elif left_distance > right_distance:
-        return left, keymap[number], "R"
-    elif right_distance > left_distance:
-        return keymap[number], right, "L"
-        
-    
-    
-    
+            return left, keypad[number], "R"
