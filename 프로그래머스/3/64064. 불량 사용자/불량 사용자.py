f638cac595998b886a4173d0df0f267671015a45
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-11-28 08:17:27 +0900

    [level 3] Title: 불량 사용자, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -10,34 +10,36 @@
 -> 3
 """
 
-from itertools import combinations, product
-from collections import defaultdict
+from itertools import product
+
 
 def solution(user_ids, banned_ids):
     candidates = [[] for _ in range(len(banned_ids))]
-    
+
     for idx, banned_id in enumerate(banned_ids):
         for user_id in user_ids:
             if len(user_id) != len(banned_id):
                 continue
-            
+
             if match(user_id, banned_id):
                 candidates[idx].append(user_id)
-                
+
     answer = set()
     for case in product(*candidates):
         if len(set(case)) == len(banned_ids):
             answer.add(frozenset(case))
-            
+
     return len(answer)
+
 
 def match(user_id, banned_id):
     user_id = list(user_id)
-    
+
     for idx in find_asterisk(banned_id):
         user_id[idx] = "*"
-        
+
     return "".join(user_id) == banned_id
+
 
 def find_asterisk(banned_id):
     return [idx for idx, char in enumerate(banned_id) if char == "*"]
