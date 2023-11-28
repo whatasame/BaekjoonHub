fbb8650dc95e099bc83b1678998df7eeb53b62f1
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-11-28 10:32:45 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.10 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -7,38 +7,56 @@
 # 직선 도로의 개수가 많아도 더 쌀 수 있음 -> 최단 경로가 답이 아닐 수 있음
 
 # 최소 비용 반환
+# 이전 노드와 다음 노드 정보가 있으면 다음 노드로 가는 비용을 알 수 있다.
+# 이전 노드와 다음 노드의 r, c가 모두 다르면 코너, 하나라도 같으면 직선
 
-import heapq
-
+from collections import deque
 
 def solution(board):
     n = len(board)
 
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    costs = [[[float("inf") for _ in range(4)] for _ in range(n)] for _ in range(n)]
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    costs = [[float("inf") for _ in range(n)] for _ in range(n)]
 
-    min_heap = [(0, 0, 0, None)]  # cost, r, c, d
+    q = deque()
+    q.append(( (0, 0), None) )
+    visited[0][0] = True
+    costs[0][0] = 0
 
-    while min_heap:
-        cost, r, c, d = heapq.heappop(min_heap)
+    while q:
+        now, bef = q.popleft()
+        # print(now, bef)
+        r, c = now
+        visited[r][c] = True
 
-        for dd, (dr, dc) in enumerate(directions):
+        for dr, dc in directions:
             nr, nc = r + dr, c + dc
 
-            # out of index
-            if not (0 <= nr < n and 0 <= nc < n):
-                continue
+            if not (0 <= nr < n and 0 <= nc < n and board[nr][nc] == 0 and not visited[nr][nc]):
+                continue 
+            new_cost = costs[r][c] + get_cost(bef, (nr, nc))
+            costs[nr][nc] = min(costs[nr][nc], new_cost)
 
-            # wall
-            if board[nr][nc] == 1:
-                continue
+            # print(f"r, c = {r} {c}, costs[{r}][{c}] = {costs[r][c]}")
+            # print(f"nr, nc = {nr} {nc}, get_cost() = {get_cost(bef, (nr, nc))}")
+            # print(f"costs[{nr}][{nc}] = {costs[nr][nc]}")
 
-            # 비용이 더 싸다면 방문
-            new_cost = cost + (100 if d == dd or d == None else 600)
-            if costs[nr][nc][dd] < new_cost:
-                continue
+            q.append(((nr, nc), (r, c)))
+            # visited[nr][nc] = True
+                
+    return costs[n-1][n-1]
+                
+def get_cost(bef, nxt):
+    if bef == None:
+        return 100
+    
+    bef_r, bef_c = bef
+    nxt_r, nxt_c = nxt
+    if bef_r != nxt_r and bef_c != nxt_c:
+        return 600
+    
+    return 100
 
-            heapq.heappush(min_heap, (new_cost, nr, nc, dd))
-            costs[nr][nc][dd] = new_cost
 
-    return min(costs[n - 1][n - 1])
+
