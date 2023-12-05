f7cc91fad4dd988ecdffb804c2c9b5edc514ee6f
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-12-05 11:43:01 +0900

    [Bronze I] Title: 피카츄, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/14405. 피카츄/피카츄.py
+++ 백준/Bronze/14405. 피카츄/피카츄.py
@@ -2,17 +2,11 @@ import sys
 
 
 def solution(s):
-    for word_pi in s.split("pi"):
-        if not word_pi:
-            continue
+    for word in ("pi", "ka", "chu"):
+        s = s.replace(word, " ")
 
-        for word_ka in word_pi.split("ka"):
-            if not word_ka:
-                continue
-
-            for word_chu in word_ka.split("chu"):
-                if word_chu.strip():
-                    return "NO"
+    if s.strip():
+        return "NO"
 
     return "YES"
 
