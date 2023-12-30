088654955a780335fe19f9fa0d1dc8268f72aa0b
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-12-30 10:15:49 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,44 @@
+# n - 1 <- n -> n + 1
+
+# 여벌을 가지고 있는 학생이 도난당한 학생일 수도 있다
+# 이 경우 여벌 학생은 못 빌려준다
+
+# n - 1을 먼저 검사하고 n + 1을 검사하면 최적이다.
+
+"""
+5
+[2, 4]
+[1, 5]
+-> 5
+
+5
+[1, 2, 4]
+[2, 3, 4, 5]
+-> 4
+"""
+
+def solution(n, lost, reserve):
+    lost, reserve = set(lost), set(reserve)
+    
+    # 여벌 가져왔으면서 도난당한 학생 제외
+    remain = reserve - lost
+    lost -= reserve
+    
+    # 1번부터 N번까지
+    answer = 0
+    for i in range(1, n + 1):
+        # 평범한 사람 체크
+        if not i in lost:
+            answer += 1
+            continue
+            
+        # i - 1, i + 1번째 순서로 체육복 빌리기
+        for k in [i - 1, i + 1]:
+            if k in remain:
+                remain.remove(k)
+                answer += 1
+                break
+                
+    return answer
+        
+            
