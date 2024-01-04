b608d432783cf17786b613ed7b2ccb350ea43df3
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-01-04 16:07:05 +0900

    [level 3] Title: 외벽 점검, Time: 830.85 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
@@ -10,7 +10,7 @@ from itertools import permutations
 
 def solution(n, weaks, dists):
     answer = float("inf")
-
+    
     # 친구 d명을 순서대로 세운다 -> O(d!)
     for case in permutations(dists):
         # 친구들을 투입해서 모두 점검했다면 해당 친구 수 리턴 -> O(weak * dist * weak)
@@ -25,7 +25,7 @@ def run(n, weaks, dists):
     friend_cnt = float("inf")
     for _ in range(len(weaks)):
         check_cnt = 0
-
+        
         # 모든 친구들에 대하여 -> O(dist)
         for friend_idx, dist in enumerate(dists):
             # 가장 가까운 취약지점부터 갈 수 있는 곳까지 모두 점검 -> O(weak)
@@ -33,12 +33,12 @@ def run(n, weaks, dists):
             for idx in range(check_cnt, len(weaks)):
                 if weaks[idx] <= end:
                     check_cnt += 1
-
+            
             # 모두다 점검했다면 사용한 친구 수 업데이트
             if check_cnt == len(weaks):
                 friend_cnt = min(friend_cnt, friend_idx + 1)
                 break
-
+            
         weaks.append(weaks.popleft() + n)
 
     return friend_cnt
