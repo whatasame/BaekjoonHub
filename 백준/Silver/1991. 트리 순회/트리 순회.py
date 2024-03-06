909c4759ae6c62cb27266e3386b6c4ca8fdfc2de
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-03-07 05:24:58 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -6,27 +6,25 @@ n = int(input())
 graph = {}
 for _ in range(n):
     node, left, right = input().split()
-    left = None if left == "." else left
-    right = None if right == "." else right
     graph[node] = (left, right)
 
 def solution(graph):
     return [preorder("A"), inorder("A"), postorder("A")]
 
 def preorder(node):
-    if not node:
+    if node == ".":
         return ""
 
     return node + preorder(graph[node][0]) + preorder(graph[node][1])
 
 def inorder(node):
-    if not node:
+    if node == ".":
         return ""
 
     return inorder(graph[node][0]) + node + inorder(graph[node][1])
 
 def postorder(node):
-    if not node:
+    if node == ".":
         return ""
 
     return postorder(graph[node][0]) + postorder(graph[node][1]) + node
