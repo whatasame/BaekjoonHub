1b895619e737da331ad4c7425d473ce0eff35b10
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-03-23 01:27:48 +0900

    [Gold V] Title: 달력, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.py
+++ 백준/Gold/20207. 달력/달력.py
@@ -1,7 +1,7 @@
 # 모든 일정을 포함하는 가장 작은 직사각형의 면적을 구하라
 
-# 가로 길이 = 가장 큰 종료일
-# 세로 길이는 전체 줄 수에 비례
+# 최종적으로 구해야하는 것은 연속된 일정에서 최대 일정 수 (세로)와 연속된 일정의 일 수 (길이)
+# 따라서, 연속된 일정은 알아서 빈 칸에 들어간다고 가정해도 무방하다.
 
 """
 5
@@ -31,45 +31,25 @@ n = int(input())
 schedules = [tuple(map(int, input().split())) for _ in range(n)]
 
 def solution():
-    max_day = max(schedules, key=lambda s:s[1])[1]
-    
-    # idx: 날짜, val: 가능한 라인
-    lines = [[0 for _ in range(max_day + 1)]] # 0 ~ max_day
-
-    # 모든 일정에 대하여 -> O(n)
-    for start, end in schedules:
-        inserted = False
-        # 들어갈 수 있는 라인 찾아서 넣기 -> O(n)
-        for line in lines:
-            if sum(line[start:end+1]) != 0:
-                continue
-            for day in range(start, end + 1):
-                line[day] = 1
-                inserted = True
-            break
-        # 다른 라인에 들어갈 수 없으면 새로운 라인 생성
-        if not inserted:
-            new_line = [0 for _ in range(max_day + 1)]
-            for day in range(start, end + 1):
-                new_line[day] = 1
-            lines.append(new_line)
+    # 날짜별 일정 개수 구하기
+    cnt = [0 for _ in range(366)] # 0 ~ 365
+    for start, end in schedules: # -> O(n)
+        for day in range(start, end + 1): # -> O(1)
+            cnt[day] += 1
 
     # 코팅지 면적 구하기
     width, height = 0, 0
     answer = 0
-    for day in range(1, max_day + 1): # 모든 날에 대하여 -> O(1)
-        # 일정 개수 구하기
-        schedule_cnt = sum(line[day] for line in lines)
-
+    for day in range(1, 366): # 모든 날에 대하여 -> O(1)
         # 일정이 없다면 코팅지 면적 더하기, 높이 0
-        if schedule_cnt == 0:
+        if cnt[day] == 0:
             answer += width * height
             width, height = 0, 0
             continue
 
         width += 1
-        height = max(height, schedule_cnt)
-        if day == max_day: # 마지막 날에는 무조건 계산
+        height = max(height, cnt[day])
+        if day == 365: # 마지막 날에는 무조건 계산
             answer += width * height
 
     return answer
