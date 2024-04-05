ec4407ebbde55c6999d3dc1145b7ea2d29db7891
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-04-05 16:30:48 +0900

    [Gold V] Title: 선발 명단, Time: 176 ms, Memory: 111820 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.py
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -8,30 +8,23 @@ input = lambda:sys.stdin.readline().strip()
 
 c = int(input())
 cases = [[list(map(int, input().split())) for _ in range(11)] for _ in range(c)]
-result = 0
 
 def solution(cases):
-    global result
-
-    answer = []
-    for case in cases:
-        result = 0
-        dfs(case, 0, set(), 0)
-        answer.append(result)
-
-    return answer
+    return [dfs(case, 0, set(), 0) for case in cases]
 
 def dfs(case, player, selected, score):
-    global result
     if player == 11:
-        result = max(result, score)
+        return score
 
+    max_score = 0
     for position in range(11):
         if position in selected or case[player][position] == 0:
             continue
 
         selected.add(position)
-        dfs(case, player + 1, selected, score + case[player][position])
+        max_score = max(max_score, dfs(case, player + 1, selected, score + case[player][position]))
         selected.remove(position)
+
+    return max_score
 
 print("\n".join(map(str, solution(cases))))
