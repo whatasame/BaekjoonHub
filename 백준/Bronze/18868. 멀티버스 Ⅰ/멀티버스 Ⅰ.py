694b53dcfa62fbc056e064d484103a2bd1bc1e47
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2024-04-27 12:36:09 +0900

    [Bronze I] Title: 멀티버스 Ⅰ, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/18868. 멀티버스 Ⅰ/멀티버스 Ⅰ.py
+++ 백준/Bronze/18868. 멀티버스 Ⅰ/멀티버스 Ⅰ.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import combinations
 
 """
 3 3
@@ -13,22 +14,23 @@ import sys
 -> 0
 """
 
+input = lambda:sys.stdin.readline().strip()
+
 m, n = map(int, input().split())
 planets = [list(map(int, input().split())) for _ in range(m)]
 
 def solution(planets):
-    # 랭킹 구하기 -> O(n^2)
+    # 랭킹 구하기 ->
     orders = []
     for planet in planets:
-        sorted_planet = sorted(planet) # O(nlogn)
-        orders.append([sorted_planet.index(e) for e in planet]) # O(n^2)
+        sorted_planet = sorted(planet)
+        orders.append([sorted_planet.index(e) for e in planet])
 
     # 순서들의 조합으로 쌍 구하기 -> O(m^2n)
     answer = 0
-    for i in range(m):
-        for j in range(i + 1, m):
-            if orders[i] == orders[j]:
-                answer += 1
+    for case in combinations(orders, 2):
+        if case[0] == case[1]:
+            answer += 1
 
     return answer
 
