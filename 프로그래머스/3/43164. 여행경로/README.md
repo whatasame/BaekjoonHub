fe2d78d87ba47eec3df138b838c15d34632fe71a
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2025-03-15 04:31:40 +0900

    [level 3] Title: 여행경로, Time: 2.03 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#) 
 
 ### 성능 요약
 
-메모리: 109 MB, 시간: 78.84 ms
+메모리: 86 MB, 시간: 2.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 15일 03:51:38
+2025년 03월 15일 04:31:38
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -8,37 +8,58 @@ import java.util.stream.*;
 
 class Solution {
     
-    String[][] tickets;
-    List<String> routes = new ArrayList<>();
+    Map<String, List<Airport>> graph = new HashMap<>();
     boolean[] visited;
-    
+    int total;
+      
     public String[] solution(String[][] tickets) {
-        this.tickets = tickets;
-        this.visited = new boolean[tickets.length];
-        
-        dfs("ICN", "ICN", 0);
-        
-        Collections.sort(routes);
-        
-        return routes.get(0).split(" ");
-    }
-    
-    void dfs(String airport, String route, int count) {
-        if (count == tickets.length) { // 모든 항공권 사용
-            routes.add(route);
-            return;
-        }
-        
         for (int i = 0; i < tickets.length; i++) {
             String from = tickets[i][0];
             String to = tickets[i][1];
             
-            if (!airport.equals(from) || visited[i]) continue;
-            
+            graph.computeIfAbsent(from, key -> new ArrayList<>()).add(new Airport(to, i));
+        }
+    
+        for (String key : graph.keySet()) {
+            Collections.sort(graph.get(key), (a1, a2) -> a1.name.compareTo(a2.name));
+        }
+        
+        List<String> route = new ArrayList<>(); 
+        route.add("ICN");
+        this.visited = new boolean[tickets.length];
+        this.total = tickets.length + 1;
+        
+        dfs("ICN", route);
+        
+        return route.toArray(String[]::new);
+    }
+    
+    boolean dfs(String current, List<String> route) {
+        if (route.size() == total) return true; // 항공권 전부 사용
+        
+        if (!graph.containsKey(current)) return false; // 현재 위치에서 출발하는 티켓 없음
+        
+        for (Airport next : graph.get(current)) {
+            if (visited[next.idx]) continue;
+
             // 백트래킹
-            visited[i] = true;
-            dfs(to, route + " " + to, count + 1);
-            visited[i] = false;
-        }            
+            visited[next.idx] = true;
+            route.add(next.name);
+            if (dfs(next.name, route)) return true; // 바로 반환
+            route.remove(route.size() - 1);
+            visited[next.idx] = false;
+        }
+        
+        return false;
+    }
+    
+    static class Airport {
+        String name;
+        int idx; // 티켓 인덱스
+        
+        Airport(String name, int idx) {
+            this.name = name;
+            this.idx = idx;
+        }
     }
 }
